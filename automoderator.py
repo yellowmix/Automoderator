93f021bea80e3ea2c63e4c1a50eb76cec01061ed
Chad Birch <chad.birch@gmail.com>
2013-10-25 20:37:01 -0600

    Add link_title match subject

--- automoderator.py
+++ automoderator.py
@@ -36,7 +36,8 @@ class Condition(object):
 
     _match_targets = ['link_id', 'user', 'title', 'domain', 'url', 'body',
                       'media_user', 'media_title', 'media_description',
-                      'author_flair_text', 'author_flair_css_class']
+                      'author_flair_text', 'author_flair_css_class',
+                      'link_title']
     _match_modifiers = {'full-exact': u'^{0}$',
                         'full-text': ur'^\W*{0}\W*$',
                         'includes': u'{0}',
